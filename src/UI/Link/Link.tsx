42faf1b27da605ca329ba83eb088cf646a3a45c2
Alexander-Agu <firstsonagu@gmail.com>
2025-10-09 06:54:03 +0200

    Fixing build error 2

--- src/UI/Link/Link.tsx
+++ src/UI/Link/Link.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "./link.css";
 
 type linkProps = {
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import Header from './components/Header/Header.tsx'
 import Hero from './components/Hero/Hero.tsx'
 import About from './components/About/About.tsx'
