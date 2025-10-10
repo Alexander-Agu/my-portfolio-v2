d80ebc3a46578f421c064f3f3d753ffa24cd919b
Alexander-Agu <firstsonagu@gmail.com>
2025-10-10 19:25:09 +0200

    Made the send email card functional

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -7,6 +7,10 @@ import { AiOutlineLinkedin } from "react-icons/ai";
 import Resume from "../../assets/Alexander_Agu_CV.pdf";
 import { useState } from "react";
 import ContactInput from "./ContactInput";
+import emailjs from "emailjs-com";
+import { motion } from "motion/react";
+
+
 
 interface Ilinks {
   link: string,
@@ -44,12 +48,64 @@ export default function Contact() {
   const [message, setMessage] = useState("");
 
 
+  const sendEmail = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    let sName = name;
+    let sEmail = email;
+    let sMessage = message;
+
+    setName("");
+    setEmail("");
+    setMessage("");
+
+
+    if (email != "" && name != "" && message != ""){
+      emailjs
+        .send(
+          "service_vpnq3m5", 
+          "template_us8cg4p",
+          {
+            name: sName,
+            email: sEmail,
+            message: sMessage
+          },
+          "sMo90MM6evdwuyMWI"  
+        )
+        .then(
+          () => {
+            alert("Message sent successfully!");
+            setName("");
+            setEmail("");
+            setMessage("");
+          },
+          (error) => {
+            alert("Something went wrong. Please try again.");
+            console.error(error.text);
+          }
+        );
+    } else{
+      alert("Please enter valid information")
+    }
+
+
+  };
+
+
 
   return (
-    <section id="contact" className="bg-black p-5 flex flex-col items-center justify-center gap-7
+    <section id="contact" className="overflow-hidden bg-black p-5 flex flex-col items-center justify-center gap-7
       lg:flex-row lg:p-[4rem] 2xl:p-[6rem]
     ">
-      <div className="w-full h-full">
+      <motion.div className="w-full h-full"
+        variants={{
+          hidden: {opacity: 0, y: -100},
+          visible: {opacity: 1, y: 0}
+        }}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{duration: 0.5, delay: 0.25}}
+      >
         <p className="text-[#ffffff8e] inter">• Contacts</p>
         <h2 className="text-[#ffffff] inter text-[3rem] lg:text-[5em] xl:text-[7em]">GET IN TOUCH</h2>
 
@@ -61,10 +117,19 @@ export default function Contact() {
             })
           }
         </div>
-      </div>
+      </motion.div>
 
 
-      <div className="w-full h-full flex items-center justify-center">
+      <motion.div className="w-full h-full flex items-center justify-center"
+        variants={{
+          hidden: {opacity: 0, y: 100},
+          visible: {opacity: 1, y: 0}
+        }}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{duration: 0.5, delay: 0.25}}
+      >
           <div className="p-2 lg:p-5 w-full flex flex-col items-start justify-center gap-5 bg-[rgb(31,31,33)]">
             <p className="text-[#ffffff] inter">Send Message</p>
 
@@ -78,11 +143,11 @@ export default function Contact() {
               <textarea onChange={(e)=>{setMessage(e.target.value)}} value={message} name="message" id="" className="w-full border-none outline-none p-2 bg-[#424242] text-[#ffffff] inter" cols={60} rows={6}></textarea>
             </div>
 
-            <button className="w-full h-[40px] p-2 bg-[#424242] outline-none border-none flex items-center justify-center text-[#ffffff] inter">
+            <button onClick={(e) => sendEmail(e)} className="hover:bg-[#353535] w-full h-[40px] p-2 bg-[#424242] outline-none border-none flex items-center justify-center text-[#ffffff] inter">
               Send Message
             </button>
           </div>
-      </div>
+      </motion.div>
       
     </section>
   )
